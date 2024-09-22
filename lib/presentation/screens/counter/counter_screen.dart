ed8f75a9608a2a24217de0969bc30ba1a4a5d948
Juan Manuel Carranza <ikkicarranza@gmail.com>
2024-09-22 12:56:39 -0400

    Adding funtionality

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
+
+
   const CounterScreen({super.key});
 
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0; 
+  String clicks="Clicks";
   @override
   Widget build(BuildContext context) {
     return   Scaffold(
       appBar: AppBar(
         title: const Text("Counter Screen"),
       ),
-        body: const Center(
+        body:  Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center ,
             children:  [
-              Text("10", style:TextStyle( fontSize: 160, fontWeight: FontWeight.w100)),
-              Text("Clicks", style:TextStyle( fontSize: 25))
+              Text(clickCounter.toString(),   style: const TextStyle( fontSize: 160, fontWeight: FontWeight.w100)),
+              Text(clicks, style: const TextStyle( fontSize: 25))
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: (){}, 
+          onPressed: (){
+            clickCounter ++;
+            if(clickCounter==1){
+              clicks="Click";
+            }else{
+              clicks="Clicks";
+            }
+            setState(() {});
+          }, 
           child: const Icon(Icons.plus_one),),
         );
   }
